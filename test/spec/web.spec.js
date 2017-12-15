25b1410a03b4fddd1bf1124d5a34596abe75229c
nakagawa <naka3kan@example.com>
2017-12-15 16:44:40 +0900

    試験を書き直して、正しく動いていることを確認した。

--- test/spec/web.spec.js
+++ test/spec/web.spec.js
@@ -1,7 +1,9 @@
 const PerfSonar = require('../../index');
+const ALLOW_TIME = 500;
 
-describe('angularjs homepage test', function() {
-  it('until show', function() {
+describe('angularjs homepage test', function () {
+  var origineTimeShow = null;
+  it('until show', function () {
     var perfSonar = new PerfSonar.sonar(browser);
     browser.get('https://docs.angularjs.org/tutorial/step_00');
 
@@ -15,37 +17,73 @@ describe('angularjs homepage test', function() {
 
     browser.sleep(1000);
 
+    element(by.tagName("button")).click();
+
     // You wrote your first test, cross it off the list
-    perfSonar.end().then(function(measureTime) {
+    perfSonar.end().then(function (measureTime) {
       console.log('性能計測結果:' + measureTime);
+      origineTimeShow = measureTime;
     });
   });
 
- it('until show 5s', function() {
+  it('until show', function () {
     var perfSonar = new PerfSonar.sonar(browser);
     browser.get('https://docs.angularjs.org/tutorial/step_00');
 
     browser.waitForAngular();
 
     perfSonar.untilShow('.alert.alert-info');
+
+    browser.sleep(1000);
+
+    element(by.css('.nav-index-group .nav-index-listing:nth-child(2) a')).click();
+
+    browser.sleep(1000);
+
+    element(by.tagName("button")).click();
+
+    browser.sleep(1000);
+
+    // You wrote your first test, cross it off the list
+    perfSonar.end().then(function (measureTime) {
+      console.log('性能計測結果:' + measureTime);
+      expect(measureTime).toBeGreaterThan(origineTimeShow - ALLOW_TIME);
+      expect(measureTime).toBeLessThan(origineTimeShow + ALLOW_TIME);
+    });
+  });
+
+  it('until show 5s', function () {
+    var perfSonar = new PerfSonar.sonar(browser);
+    browser.get('https://docs.angularjs.org/tutorial/step_00');
+
+    browser.waitForAngular();
+
+    perfSonar.untilShow('.alert.alert-info');
+
+    browser.sleep(1000);
+
+    element(by.css('.nav-index-group .nav-index-listing:nth-child(2) a')).click();
 
     browser.sleep(5000);
 
-    element(by.css('.nav-index-group .nav-index-listing:nth-child(2) a')).click();
+    element(by.tagName("button")).click();
 
-    browser.sleep(1000);
     // You wrote your first test, cross it off the list
-    perfSonar.end().then(function(measureTime) {
+    perfSonar.end().then(function (measureTime) {
       console.log('性能計測結果:' + measureTime);
+      expect(measureTime).toBeGreaterThan(origineTimeShow + 4000 - ALLOW_TIME);
+      expect(measureTime).toBeLessThan(origineTimeShow + 4000 + ALLOW_TIME);
     });
   });
 
-
-  it('until hide', function() {
+  var origineTimeHide = null;
+  it('until hide', function () {
     var perfSonar = new PerfSonar.sonar(browser);
     browser.get('https://docs.angularjs.org/tutorial/step_01');
 
     browser.waitForAngular();
+
+    element(by.tagName("button")).click();
 
     perfSonar.untilHide('.alert.alert-info');
 
@@ -53,18 +91,20 @@ describe('angularjs homepage test', function() {
 
     element(by.css('.nav-index-group .nav-index-listing:nth-child(1) a')).click();
 
-    browser.sleep(1000);
     // You wrote your first test, cross it off the list
-    perfSonar.end().then(function(measureTime) {
+    perfSonar.end().then(function (measureTime) {
       console.log('性能計測結果:' + measureTime);
+      origineTimeHide = measureTime;
     });
   });
 
-  it('until hide 5s', function() {
+  it('until hide 5s', function () {
     var perfSonar = new PerfSonar.sonar(browser);
     browser.get('https://docs.angularjs.org/tutorial/step_01');
 
     browser.waitForAngular();
+
+    element(by.tagName("button")).click();
 
     perfSonar.untilHide('.alert.alert-info');
 
@@ -72,10 +112,11 @@ describe('angularjs homepage test', function() {
 
     element(by.css('.nav-index-group .nav-index-listing:nth-child(1) a')).click();
 
-    browser.sleep(1000);
     // You wrote your first test, cross it off the list
-    perfSonar.end().then(function(measureTime) {
+    perfSonar.end().then(function (measureTime) {
       console.log('性能計測結果:' + measureTime);
+      expect(measureTime).toBeGreaterThan(origineTimeHide + 5000 - ALLOW_TIME);
+      expect(measureTime).toBeLessThan(origineTimeHide + 5000 + ALLOW_TIME);
     });
   });
 });
